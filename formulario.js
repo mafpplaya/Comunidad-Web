f2be1aadbbf1fc62dfde8a3bb424c3c599220374
mafpplaya <mflores@dotnet.com.mx>
2015-03-08 09:35:46 -0500

    Trabajo con el formulario

--- formulario.js
+++ formulario.js
@@ -2,38 +2,99 @@ var $form = $('#formulario'),
 	$titulo = $('#titulo'),
 	$url = $('#url'),
 	/*$button = $('#mostrar-form'),*/
-	$lista = $('#contenido'),
-	$primerPost = $('.item').first();
+	$primerPost = $('.item').first(),
+	$lista = $('#contenido');
 
-function mostrarOcultarFormulario()
+if(localStorage.getItem('autosave'))
 {
-	$form.slideToggle();
-	$lista.slideToggle();
-	return false;
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
 }
 
-function agregarPost()
-{
-	var url = $url.val(),
-		titulo = $titulo.val(),
-		$clone = $primerPost.clone();
+var id = setInterval(function()
+	{
+		sessionStorage.setItem('titulo', $titulo.val());
+		sessionStorage.setItem('url', $url.val());
+	}, 
+	1000);
 
-	$clone.find('.titulo_item a')
+function mostrarOcultarFormulario(evento)
+{
+	evento.preventDefault();
+	evento.stopPropagation();
+	$form.slideToggle();
+	$lista.slideToggle();
+	/*return false;*/
+}
+
+function agregarPost(evento)
+{
+	evento.preventDefault();
+	evento.stopPropagation();
+	var titulo = $titulo.val(),
+		url = $url.val(),
+		//$clone = $primerPost.clone();
+		clone = $primerPost.clone();
+
+	//$clone.find('.titulo_item a')
+	clone.find('.titulo_item a')
 		.text(titulo)
 		.attr('href', url);
 
-	$clone.hide()
+	//$clone.hide()
+	clone.hide();
 
-	$lista.prepend($clone);
+	$lista.prepend(clone/*$clone*/);
 
 	mostrarOcultarFormulario();
-	$clone.slideDown();
+    $titulo.val('');
+    $url.val('');
 
-	return false;
+	//$clone.slideDown();
+	clone.slideDown();
+
+	/*return false;*/
 }
+
+function grabarInformacion(evento)
+{
+	evento.preventDefault();
+
+	var titulo = $titulo.val(),
+		url	   = $url.val(),
+		ls     = localStorage,
+		ss 	   = sessionStorage;
+
+	ls.setItem('titulo', titulo);
+	ls.setItem('url', url);
+
+	ss.setItem('titulo', titulo);
+	ss.setItem('url', url);
+
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");	
+}
+
 
 	//Eventos (Escuchar)
 	/*$form.on('submit', agregarPost);
 	$button.click(mostrarOcultarFormulario);*/
 	$('#publicar_nav a').click(mostrarOcultarFormulario);
 	$('#formulario').on('submit', agregarPost);
+
+	/*$('#formulario')   muestra texto en un input
+		.on('submit', agregarPost)
+		.find('#url')
+		.on('focus', function()
+			{
+				$('#url').val('http://');
+			})
+		.on('blur', function()
+			{
+				$('#url').val('');
+			});*/
+
+/**/
+$('nav').on('click', function(){console.log("Soy un nav y me hicieron click");});
+$('nav ul').on('click', function(){console.log("Soy un ul y me hicieron click");});
